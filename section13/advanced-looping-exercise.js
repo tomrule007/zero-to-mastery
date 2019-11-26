73fd6a008b916476a313dc745aee2755e9cf1e77
Thomas Herzog <Tomrule007@gmail.com>
2019-11-26 08:09:48 -0800

    Section 13 advanced-looping w/ solutions file

---
+++ section13/advanced-looping-exercise.js
@@ -0,0 +1,69 @@
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+};
+
+//1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+//2
+basket.forEach(item => {
+  console.log(item);
+});
+
+for (item in detailedBasket) {
+  console.log(item);
+}
+
+for (item of basket) {
+  console.log(item);
+}
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1, 0, 3, 100, 99, 2, 99]; // should return 100
+const array2 = ['a', 3, 4, 2]; // should return 4
+const array3 = []; // should return 0
+//!* In production I would use biggestNumberInArray = arr => Math.max(...arr)
+function biggestNumberInArray(arr) {
+  let max;
+  for (let i = 0, { length } = arr; i < length; i++) {
+    let value = arr[i];
+    if (typeof value === 'number' && (max === undefined || value > max))
+      max = arr[i];
+  }
+  return max || 0;
+}
+
+function biggestNumberInArray2(arr) {
+  let max;
+  for (let value of arr) {
+    if (typeof value === 'number' && (max === undefined || value > max))
+      max = value;
+  }
+  return max || 0;
+}
+
+function biggestNumberInArray3(arr) {
+  if (!arr.length) return 0;
+  return arr
+    .filter(x => typeof value === 'number')
+    .reduce((max, value) => (value > max ? value : max));
+}
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+const amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+};
+
+const checkBasket = (basket, lookingFor) => lookingFor in basket;
