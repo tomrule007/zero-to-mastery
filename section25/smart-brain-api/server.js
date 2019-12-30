33cdcf56a68ecabb765609b09dff5fd6b56de6d8
Thomas Herzog <Tomrule007@gmail.com>
2019-12-30 08:11:12 -0800

    Use knex.transaction to store login pass hash

--- section25/smart-brain-api/server.js
+++ section25/smart-brain-api/server.js
@@ -73,10 +73,21 @@ app.post('/register', (req, res) => {
   console.log({ name, email, password });
   bcrypt.hash(password, saltRounds).then(hash => {
     console.log({ hash });
-    knex('users')
-      .returning('*')
-      .insert({ email, name, joined: new Date() })
-      .then(newUser => res.json(newUser))
+    knex
+      .transaction(trx => {
+        trx
+          .insert({ hash, email })
+          .into('login')
+          .returning('email')
+          .then(loginEmail =>
+            trx('users')
+              .returning('*')
+              .insert({ email: loginEmail[0], name, joined: new Date() })
+              .then(newUser => res.json(newUser))
+          )
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
       .catch(err => {
         console.log(err);
         res.status(400).json('failed to register');
