c40bf098451750f0d4e01e2416bd6a16838a7df3
Thomas Herzog <Tomrule007@gmail.com>
2019-12-31 07:57:08 -0800

    Refactor - created /register endpoint controller

--- section25/smart-brain-api/server.js
+++ section25/smart-brain-api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const register = require('./controllers/register');
 const dotenvConfiged = require('dotenv').config();
 const knex = require('knex')({
   client: 'pg',
@@ -72,32 +73,7 @@ app.post('/signin', (req, res) => {
       res.status(400).json('Invalid Password or Email');
     });
 });
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  console.log({ name, email, password });
-  bcrypt.hash(password, saltRounds).then(hash => {
-    console.log({ hash });
-    knex
-      .transaction(trx => {
-        trx
-          .insert({ hash, email })
-          .into('login')
-          .returning('email')
-          .then(loginEmail =>
-            trx('users')
-              .returning('*')
-              .insert({ email: loginEmail[0], name, joined: new Date() })
-              .then(newUser => res.json(newUser[0]))
-          )
-          .then(trx.commit)
-          .catch(trx.rollback);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json('failed to register');
-      });
-  });
-});
+app.post('/register', register.handleRegister(bcrypt, saltRounds, knex));
 app.get('/profile/:userId', (req, res) => {
   const { userId } = req.params;
   knex('users')
